77370d6ac61e9340466ff6b2ff05191fd9625046
Léo Brückl <leo.bruckl@epfl.ch>
2022-03-22 17:40:27 +0100

    test

--- src/nutrition.h
+++ src/nutrition.h
@@ -4,4 +4,24 @@
  \date     22.03.2022
  \brief    nutrition interface
 *****************************************************************/
+ #ifndef NUTRITION_H
+ #define NUTRITION_H
+
+ class Nutrition
+ {
+     private:
+        int x,y;
+     public:
+
+ };
+
+ int i=0;
+
+#endif
+ 
+
+
+
+
+
  
