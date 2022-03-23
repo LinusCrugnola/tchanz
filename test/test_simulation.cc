7af200cf7b299fd32da1914482563134c1a14987
Linus Crugnola <linus.crugnola@epfl.ch>
2022-03-23 22:22:10 +0100

    simulation testfile added

---
+++ test/test_simulation.cc
@@ -0,0 +1,15 @@
+/****************************************************************!
+ \file     test_simulation.cc
+ \author   Léo Brückl, Linus Crugnola
+ \date     23.03.2022
+ \brief    test_simulation implementation
+*****************************************************************/
+ 
+#include <cassert>
+
+#include "../src/simulation.h"
+
+int main(){
+
+    return 0;
+}
