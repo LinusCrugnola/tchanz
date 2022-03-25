d2f031aa75c2daace910a9a71cc49b45c80ccfa5
Linus Crugnola <linus.crugnola@epfl.ch>
2022-03-25 20:24:19 +0100

    added ant classes

--- src/ants.h
+++ src/ants.h
@@ -5,3 +5,58 @@
  \brief    ants interface
 *****************************************************************/
  
+#ifndef ANTS_H
+#define ANTS_H
+
+#include <string>
+
+#include "squarecell.h"
+
+class Ant
+{
+private:
+    square square;
+    unsigned age;
+public:
+    static Ant data_validation(std::string line);
+    Ant(square square) : square(square){};
+
+    // debug function:
+    virtual square print_data(Ant& ant);
+};
+
+// Subclasses of ant:
+class Generator : public Ant
+{
+private:
+    /* data */
+public:
+    /* data */
+};
+
+class Collector : public Ant
+{
+private:
+    /* data */
+public:
+    /* data */
+};
+
+class Defensor : public Ant
+{
+private:
+    /* data */
+public:
+    /* data */
+};
+
+class  : public Ant
+{
+private:
+    /* data */
+public:
+    /* data */
+};
+
+
+#endif
