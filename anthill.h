93b42ab4e20c64e4073a5d05b5c2817e0a00f6ce
Léo Brückl <leo.bruckl@epfl.ch>
2022-03-31 18:01:38 +0200

    anthill.h clean

--- anthill.h
+++ anthill.h
@@ -6,7 +6,7 @@
 *****************************************************************/
  
 #ifndef ANTHILL_H
- #define ANTHILL_H
+#define ANTHILL_H
  
 #include <vector>
 #include <iostream>
@@ -24,35 +24,28 @@ class Anthill
         unsigned total_food;
         unsigned nbC, nbD, nbP;
 
-        //Ants:
-        //FIXME: how can we have only one generator (error)
-        //Generator generator;
         std::vector<Generator> generator;
         std::vector<Collector> collectors;
         std::vector<Defensor> defensors;
         std::vector<Predator> predators;
     
-        //Formule de sizeF = partie_entière(sqrt(4*(sizeG*sizeG+sizeC*sizeC*nbC+sizeD*sizeD*nbD+sizeP*sizeP*nbP)))
-        //(sizeF+2=side...?)
-        //Constructeur sizeF à faire
     public:
-        /*Anthill(int sizeF) : sizeF(sizeF)
-        { 
-            //sizeF formula
-        }*/
         square get_anthill_type() const
         {
             return anthill_type;
         }
 
-
-        static Anthill anthill_validation(std::istringstream& data,std::vector<Anthill> anthills, unsigned home);
-        unsigned anthill_get_ants();
+        static Anthill anthill_validation(std::istringstream& data,
+                                        std::vector<Anthill> anthills, unsigned home);
+        unsigned anthill_get_ants(); 
         void ant_validation(std::istringstream& data, unsigned home);
 
-        Anthill(square anthill_type, unsigned total_food, unsigned nbC, unsigned nbD, unsigned nbP, unsigned xg, unsigned yg, unsigned home) : anthill_type(anthill_type), 
+        Anthill(square anthill_type, unsigned total_food, unsigned nbC, unsigned nbD,
+                unsigned nbP, unsigned xg, unsigned yg, unsigned home)
+                : anthill_type(anthill_type),
         total_food(total_food), nbC(nbC), nbD(nbD), nbP(nbP){
-            this->generator.push_back(Generator::data_validation(xg, yg, anthill_type, home));
+            this->generator.push_back(Generator::data_validation(xg, yg, anthill_type,
+                                                                home));
         }
 
 
