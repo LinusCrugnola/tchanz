40dd2ea36a4daca869b93a43f3ae2e343477e5ee
Linus Crugnola <linus.crugnola@epfl.ch>
2022-04-01 20:33:07 +0200

    nutrition approved

--- nutrition.cc
+++ nutrition.cc
@@ -1,27 +1,38 @@
 /******************************************************************************!
-* @file     nutrition.cc
-* @author   Léo Brückl <leo.bruckl@epfll.ch>            70 %
-*           Linus Crugnola <linus.crugnola@epfl.ch>     30 %
-* @version  1.0
-* @date     01.04.2022
-*******************************************************************************/
+ * @file     nutrition.cc
+ * @author   Léo Brückl <leo.bruckl@epfll.ch>            70 %
+ *           Linus Crugnola <linus.crugnola@epfl.ch>     30 %
+ * @version  1.0
+ * @date     01.04.2022
+ *******************************************************************************/
 
 #include "nutrition.h"
+
+#include <iostream>
+
+#include "error_squarecell.h"
 #include "message.h"
 
 using namespace std;
 
-Nutrition* Nutrition::data_validation(istringstream& data){
+Nutrition::Nutrition(unsigned x_coor, unsigned y_coor)
+    : x(x_coor), y(y_coor) {
+    square_add({x, y, 1, 1});
+}
+
+Nutrition* Nutrition::data_validation(istringstream& data) {
     Nutrition* ptr = nullptr;
     unsigned x_coor, y_coor;
-    if(!(data >> x_coor >> y_coor)) cout << "reading error!" << endl;
-    square_validation({x_coor,y_coor,3,1}); // throws error if invalid
-    if(square_superposition({x_coor,y_coor,1,1})){
-        cout << message::food_overlap(x_coor,y_coor) ;
+    if (!(data >> x_coor >> y_coor)) cout << "reading error!" << endl;
+
+    square_validation({x_coor, y_coor, 3, 1}); // throws error if invalid
+
+    if (square_superposition({x_coor, y_coor, 1, 1})) {
+        cout << message::food_overlap(x_coor, y_coor);
         exit(EXIT_FAILURE);
     }
+
+    // Generate nutrition
     ptr = new Nutrition(x_coor, y_coor);
     return ptr;
 }
-
-
--- nutrition.h
+++ nutrition.h
@@ -1,49 +1,48 @@
 /******************************************************************************!
-* @file     nutrition.h
-* @author   Léo Brückl <leo.bruckl@epfll.ch>            
-*           Linus Crugnola <linus.crugnola@epfl.ch>     
-* @version  1.0
-* @date     01.04.2022
-*******************************************************************************/
+ * @file     nutrition.h
+ * @author   Léo Brückl <leo.bruckl@epfll.ch>
+ *           Linus Crugnola <linus.crugnola@epfl.ch>
+ * @version  1.0
+ * @date     01.04.2022
+ *******************************************************************************/
+
 #ifndef NUTRITION_H
 #define NUTRITION_H
- 
-#include <vector>
-#include <iostream>
-#include <sstream>
-#include <string>
 
-#include "error_squarecell.h"
+#include <sstream>
+
 #include "squarecell.h"
 
-class Nutrition
-{
-    private:
-        unsigned x,y;
-        double val_food;
-    public:
-        /**
-         * @brief Verification if coordinates fit in the grid and if it has a distance
-         * bigger than 1 from the border. 
-         * Also verifies if it overlaps with another entity.
-         */
-        static Nutrition* data_validation(std::istringstream& data);
+/**
+ * @class Nutrition
+ * 
+ * @brief Nutrition objects are points in the grid that can be collected by collector 
+ * ants
+ */
+class Nutrition {
+private:
+    unsigned x, y;
+    double val_food;
 
-        /**
-         * @brief Construct a new Nutrition object
-         */        
-        Nutrition(unsigned x_coor, unsigned y_coor) : x(x_coor) , y(y_coor)
-        {
-            square_add({x, y, 1, 1});
-        }
-        ~Nutrition(){square_delete({this->x, this->y, 1,0});}
+public:
+    /**
+     * @brief Verification if coordinates fit in the grid and if it has a distance
+     * bigger than 1 from the border.
+     * Also verifies if food overlaps with another entity.
+     * 
+     * @param data input string stream
+     */
+    static Nutrition* data_validation(std::istringstream& data);
+
+    /**
+     * @brief Construct a new Nutrition object
+     */
+    Nutrition(unsigned x_coor, unsigned y_coor);
+
+    /**
+     * @brief Destroy the Nutrition object
+     */
+    ~Nutrition() {square_delete({this->x, this->y, 1, 0});}
 };
 
 #endif
- 
-
-
-
-
-
- 
