61770aaf914bbf52aebb74cafbf361c4aa40abb6
Linus Crugnola <linus.crugnola@epfl.ch>
2022-04-08 16:16:44 +0200

    Revert "Merge commit 'c9c92dbfce51744e78f66ea22086ad99f2536c53' into main"
    This reverts commit a93250decb94ee7850e4075e8e20df4a7709bf77, reversing
    changes made to fb71b901f418e4dbda7bb79a4f794e02ae3e7dad.

---
+++ test_nutrition.cc
@@ -0,0 +1,33 @@
+/******************************************************************************!
+* @file     test_nutrition.cc
+* @author   Léo Brückl <leo.bruckl@epfl.ch>             
+*           Linus Crugnola <linus.crugnola@epfl.ch>     
+* @version  1.0
+* @date     01.04.2022
+*******************************************************************************/
+
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+#include "nutrition.h"
+#include "squarecell.h"
+
+using namespace std;
+
+int main(){
+    // Test constructor:
+    cout << "Test nutrition" << endl;
+    Nutrition nutri(1,1);
+    assert(square_superposition({1,1,1,1}));
+    cout << "Test 1 passed" << endl;
+
+    // Test data validation
+    string line = " 20 55";
+    istringstream data(line);
+    Nutrition::data_validation(data);
+    assert(square_superposition({20,55,1,0}));
+    cout << "Test 2 passed" << endl;
+
+}
---
+++ test_simulation.cc
@@ -0,0 +1,16 @@
+/******************************************************************************!
+* @file     test_simulation.cc
+* @author   Léo Brückl <leo.bruckl@epfl.ch>             
+*           Linus Crugnola <linus.crugnola@epfl.ch>     
+* @version  1.0
+* @date     01.04.2022
+*******************************************************************************/
+ 
+#include <cassert>
+
+#include "simulation.h"
+
+int main(){
+
+    return 0;
+}
