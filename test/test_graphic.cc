d3f08487e1c2b95d174099cad9c82841484a61b7
Linus Crugnola <linus.crugnola@epfl.ch>
2022-04-22 14:54:44 +0200

    adjusted screen_size in test_graphic

--- test/test_graphic.cc
+++ test/test_graphic.cc
@@ -16,15 +16,17 @@ bool MockArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr){
     const double width = allocation.get_width();
     const double height = allocation.get_height();
 
-    double scalex = width/scl::g_max;
-    double scaley = width/scl::g_max;
+    //keep the aspect ratio
+    double scale_y = width/scl::g_max;
+    double delta_x = scl::g_max * (width/height);
+    double scale_x = width/delta_x;
+    double scale = scale_x < scale_y ? scale_x : scale_y;
 
     //transform to bottom left corner:
     cr->translate(0, height);
-    cr->scale(scalex, -3.9);
+    cr->scale(scale, -scale);
 
     graphic::set_context(cr);
-    graphic::draw_world(scl::g_max, width, height);
 
     //set global line width
     cr->set_line_width(1);
@@ -36,6 +38,8 @@ bool MockArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr){
 
     graphic::draw_uniform(50,50,30,1);
     graphic::draw_uniform(35,35,5,0);
+
+    graphic::draw_world(scl::g_max, width, height);
 
     return true;
 }
