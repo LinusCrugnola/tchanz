b0e974f2f70b9133f55488846ed628f824598d7e
Linus Crugnola <linus.crugnola@epfl.ch>
2022-04-23 15:32:22 +0200

    create graphic test

--- test/test_graphic.cc
+++ test/test_graphic.cc
@@ -3,6 +3,7 @@
 #include "../graphic.h"
 #include "../squarecell.h"
 #include <iostream>
+#include"../simulation.h"
 
 MockArea::MockArea(){}
 MockArea::~MockArea(){}
@@ -31,41 +32,46 @@ bool MockArea::on_draw(const Cairo::RefPtr<Cairo::Context>& cr){
     //set global line width
     cr->set_line_width(1);
 
+    //test simulation
+    Simulation simu_test;
+    simu_test.read_configfile("../testfiles/t01.txt");
+    std::cout << "drawing output: " << simu_test.draw_current_state() << std::endl;
+
     // test drawing functions
-    graphic::color color = graphic::get_new_color();
-    graphic::draw_empty(2,2,3,1,color);
-    color = graphic::get_new_color();
-    graphic::draw_empty(4,2,3,0,color);
-    color = graphic::get_new_color();
-    graphic::draw_empty(6,2,1,1,color);
-    color = graphic::get_new_color();
-    graphic::draw_empty(8,1,5,0,color);
+    // graphic::color color = graphic::get_new_color();
+    // graphic::draw_empty(2,2,3,1,color);
+    // color = graphic::get_new_color();
+    // graphic::draw_empty(4,2,3,0,color);
+    // color = graphic::get_new_color();
+    // graphic::draw_empty(6,2,1,1,color);
+    // color = graphic::get_new_color();
+    // graphic::draw_empty(8,1,5,0,color);
 
-    color = graphic::get_new_color();
-    graphic::draw_rhomb(15,2,1,1,color);
-    color = graphic::get_new_color();
-    graphic::draw_rhomb(18,2,3,1,color);
-    color = graphic::get_new_color();
-    graphic::draw_rhomb(21,2,1,0,color);
+    // color = graphic::get_new_color();
+    // graphic::draw_rhomb(15,2,1,1,color);
+    // color = graphic::get_new_color();
+    // graphic::draw_rhomb(18,2,3,1,color);
+    // color = graphic::get_new_color();
+    // graphic::draw_rhomb(21,2,1,0,color);
 
-    color = graphic::get_new_color();
-    graphic::draw_uniform(3,8,3,1,color);
-    color = graphic::get_new_color();
-    graphic::draw_uniform(7,8,5,0,color);
-    color = graphic::get_new_color();
-    graphic::draw_uniform(14,8,1,1,color);
-    color = graphic::get_new_color();
-    graphic::draw_uniform(16,8,1,0,color);
+    // color = graphic::get_new_color();
+    // graphic::draw_uniform(3,8,3,1,color);
+    // color = graphic::get_new_color();
+    // graphic::draw_uniform(7,8,5,0,color);
+    // color = graphic::get_new_color();
+    // graphic::draw_uniform(14,8,1,1,color);
+    // color = graphic::get_new_color();
+    // graphic::draw_uniform(16,8,1,0,color);
     
-    color = graphic::get_new_color();
-    graphic::draw_diagonal(3,18,3,1,color);
-    color = graphic::get_new_color();
-    graphic::draw_diagonal(9,18,3,0,color);
+    // color = graphic::get_new_color();
+    // graphic::draw_diagonal(3,18,3,1,color);
+    // color = graphic::get_new_color();
+    // graphic::draw_diagonal(9,18,3,0,color);
 
-    color = graphic::get_new_color();
-    graphic::draw_grille(15,18,3,1,color);
-    color = graphic::get_new_color();
-    graphic::draw_grille(20,18,3,0,color);
+    // color = graphic::get_new_color();
+    // graphic::draw_grille(15,18,3,1,color);
+    // color = graphic::get_new_color();
+    // graphic::draw_grille(20,18,3,0,color);
 
     graphic::draw_world(scl::g_max, width, height);
 
