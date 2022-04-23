fe54ffba5e998fc80351a99a2bbb741c50977824
Linus Crugnola <linus.crugnola@epfl.ch>
2022-04-23 22:21:17 +0200

    changed test

--- test.cpp
+++ test.cpp
@@ -1,5 +1,5 @@
 #include "test/testfunctions.h"
-//#include "GUIgraphic.h"
+#include "GUIgraphic.h"
 #include "graphic.h"
 #include <gtkmm/application.h>
 #include <gtkmm/window.h>
@@ -9,31 +9,31 @@
 
 int main(int argc, char** argv){
     std::cout << "\n------------------------- Start tests ------------------------------" << std::endl;
-    //std::cout << "\n------------------------- Test squarecell ------------------------------" << std::endl;
-    //test_squarecell();
-    //test_simulation();
-    //test_nutrition();
-    //std::cout << "\n------------------------- Test GUI ------------------------------" << std::endl;
-    
-    // auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
-	// Gui gui;
-	// gui.set_default_size(200,200);
-	
-	// return app->run(gui);
-
-    std::cout << "\n------------------------- Test Graphic ------------------------------" << std::endl;
+    // std::cout << "\n------------------------- Test squarecell ------------------------------" << std::endl;
+    // test_squarecell();
+    // test_simulation();
+    // test_nutrition();
+    std::cout << "\n------------------------- Test GUI ------------------------------" << std::endl;
     
     auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+	Gui gui;
+	gui.set_default_size(800,600);
 	
-    Gtk::Window win;
-    win.set_title("Test Graphic");
-	win.set_default_size(700,700);
+	return app->run(gui);
 
-	MockArea canvas;
-    win.add(canvas);
-    canvas.show();
+    // std::cout << "\n------------------------- Test Graphic ------------------------------" << std::endl;
+    
+    // auto app = Gtk::Application::create(argc, argv, "org.gtkmm.example");
+	
+    // Gtk::Window win;
+    // win.set_title("Test Graphic");
+	// win.set_default_size(700,700);
 
-	return app->run(win);
+	// MockArea canvas;
+    // win.add(canvas);
+    // canvas.show();
+
+	// return app->run(win);
 
     std::cout << "------------------------- Finished tests ------------------------------\n" << std::endl;
 }
