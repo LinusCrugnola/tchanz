2aba09ab7fbc33956a2f2a2f0ea2bb8e11e7f5b9
Linus Crugnola <linus.crugnola@epfl.ch>
2022-04-30 16:23:05 +0200

    gui formatted

--- gui.cc
+++ gui.cc
@@ -5,7 +5,7 @@
 * @version  1.0
 * @date     29.04.2022
 *******************************************************************************/
- 
+
 #include "gui.h"
 
 #include <iostream>
@@ -28,9 +28,8 @@ Gui::Gui()
       general_frame("General"), info_frame("Info"), anthill_info_frame("Anthill info"),
       nbF_info("Nb food:   nbF"), anthill_info("None selected          "),
       exit("exit"), open("open"), save("save"), start("start"), step("step"),
-      previous("previous"), next("next"), simulation(nullptr), timer_val(0), 
-      start_state(b_start), timeout_value(750), timer_disconnect(false)
-    {
+      previous("previous"), next("next"), simulation(nullptr), timer_val(0),
+      start_state(b_start), timeout_value(750), timer_disconnect(false) {
     set_title("Tchanz");
     set_border_width(5);
 
@@ -40,23 +39,23 @@ Gui::Gui()
 
     canvas_box.pack_start(canvas);
 
-    V_box.pack_start(general_frame,false,false);
-    V_box.pack_start(info_frame,false,false);
-    V_box.pack_start(anthill_info_frame,false,false);
+    V_box.pack_start(general_frame, false, false);
+    V_box.pack_start(info_frame, false, false);
+    V_box.pack_start(anthill_info_frame, false, false);
 
     general_frame.add(navbar);
 
     navbar.pack_start(exit);
-    exit.set_size_request(0,60);
+    exit.set_size_request(0, 60);
     navbar.pack_start(open);
-    open.set_size_request(0,60);
+    open.set_size_request(0, 60);
     navbar.pack_start(save);
-    save.set_size_request(0,60);
+    save.set_size_request(0, 60);
     navbar.pack_start(start);
-    start.set_size_request(0,60);
+    start.set_size_request(0, 60);
     navbar.pack_start(step);
-    step.set_size_request(0,60);
-    
+    step.set_size_request(0, 60);
+
     info_frame.add(nbF_info);
 
     anthill_info_frame.add(anthill_info_box);
@@ -68,11 +67,11 @@ Gui::Gui()
     exit.signal_clicked().connect(sigc::mem_fun(*this, &Gui::on_button_clicked_exit));
     open.signal_clicked().connect(sigc::mem_fun(*this, &Gui::on_button_clicked_open));
     save.signal_clicked().connect(sigc::mem_fun(*this, &Gui::on_button_clicked_save));
-    start.signal_clicked().connect(sigc::mem_fun(*this, 
+    start.signal_clicked().connect(sigc::mem_fun(*this,
                                                  &Gui::on_button_clicked_start));
     step.signal_clicked().connect(sigc::mem_fun(*this, &Gui::on_button_clicked_step));
     next.signal_clicked().connect(sigc::mem_fun(*this, &Gui::on_button_clicked_next));
-    previous.signal_clicked().connect(sigc::mem_fun(*this, 
+    previous.signal_clicked().connect(sigc::mem_fun(*this,
                                                     &Gui::on_button_clicked_previous));
 
     show_all_children();
@@ -81,36 +80,36 @@ Gui::Gui()
 Gui::~Gui() {}
 
 void Gui::update_nbF() {
-    if(this->simulation == nullptr){
+    if (this->simulation == nullptr) {
         this->nbF_info.set_text("Nb food:   0");
     }
     else {
-        this->nbF_info.set_text("Nb food:   " + 
+        this->nbF_info.set_text("Nb food:   " +
                                 to_string(this->simulation->get_nbF()));
     }
 }
 
 void Gui::draw_simulation_state() {
-    if(this->simulation != nullptr)
+    if (this->simulation != nullptr)
         this->simulation->draw_current_state();
 }
 
-unsigned Gui::get_dimension(){
-    if(this->simulation == nullptr) return 128;
+unsigned Gui::get_dimension() {
+    if (this->simulation == nullptr) return 128;
     return this->simulation->get_dimension();
 }
 
-void Gui::set_simulation(Simulation* simulation){
+void Gui::set_simulation(Simulation* simulation) {
     this->simulation = simulation;
 }
 
-void Gui::display_next_hill(bool reverse = false){
+void Gui::display_next_hill(bool reverse = false) {
     std::string info = this->simulation->get_next_anthill_info(reverse);
     this->anthill_info.set_text(info);
 }
 
-void Gui::reset_anthill_info(){
-    std::string info = this->simulation->get_next_anthill_info(0,1);
+void Gui::reset_anthill_info() {
+    std::string info = this->simulation->get_next_anthill_info(0, 1);
     this->anthill_info.set_text(info);
 }
 
@@ -119,8 +118,7 @@ Canvas::Canvas(Gui* parent)
 
 Canvas::~Canvas() {}
 
-bool Canvas::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
-{
+bool Canvas::on_draw(const Cairo::RefPtr<Cairo::Context>& cr) {
     unsigned g_max = parent->get_dimension();
     cr->set_source_rgb(0, 0, 0);
     cr->paint();
@@ -138,10 +136,10 @@ bool Canvas::on_draw(const Cairo::RefPtr<Cairo::Context>& cr)
     cr->translate(0, height);
     cr->scale(scale, -scale);
     if (scale_x > scale_y) {
-        cr->translate((width - height) / (2 * scale) -0.5 , -0.5);
+        cr->translate((width - height) / (2 * scale) - 0.5, -0.5);
     }
     else {
-        cr->translate(-0.5, (height - width) / (2 * scale) -0.5 );
+        cr->translate(-0.5, (height - width) / (2 * scale) - 0.5);
     }
 
     cr->translate(0.5, 0.5);
@@ -220,29 +218,29 @@ void Gui::on_button_clicked_save() {
 }
 
 void Gui::on_button_clicked_start() {
-	if(start_state == b_start){
+    if (start_state == b_start) {
         Glib::signal_timeout().connect(sigc::mem_fun(*this, &Gui::on_timeout),
-                                       timeout_value );
+                                       timeout_value);
         timer_disconnect = false;
         timer_val++;
         start_state = b_stop;
         this->start.set_label("Stop");
-	}
-	else{
-		timer_val--;
+    }
+    else {
+        timer_val--;
         timer_disconnect = true;
-		start_state = b_start;
-		this->start.set_label("Start");
-	}
+        start_state = b_start;
+        this->start.set_label("Start");
+    }
 }
 
 void Gui::on_button_clicked_step() {
-	if(start_state == b_start){
+    if (start_state == b_start) {
         timer_disconnect = false;
         timer_disconnect = true;
         timer_val++;
         std::cout << "This is simulation update number : " << timer_val << std::endl;
-	}    
+    }
 }
 
 void Gui::on_button_clicked_next() {
@@ -255,9 +253,9 @@ void Gui::on_button_clicked_previous() {
     this->canvas.queue_draw();
 }
 
-bool Gui::on_key_press_event(GdkEventKey * key_event){
-    if(key_event->type == GDK_KEY_PRESS){
-        switch(gdk_keyval_to_unicode(key_event->keyval)){
+bool Gui::on_key_press_event(GdkEventKey* key_event) {
+    if (key_event->type == GDK_KEY_PRESS) {
+        switch (gdk_keyval_to_unicode(key_event->keyval)) {
             case 'q':
                 on_button_clicked_exit();
                 break;
@@ -280,19 +278,19 @@ bool Gui::on_key_press_event(GdkEventKey * key_event){
     return Gtk::Window::on_key_press_event(key_event);
 }
 
-bool Gui::on_timeout(){
-    if(timer_disconnect){
+bool Gui::on_timeout() {
+    if (timer_disconnect) {
         timer_disconnect = false;
         return false;
     }
 
     std::cout << "This is simulation update number : " << timer_val << std::endl;
     ++timer_val;
-    
+
     // A call to make a single update of the simulation is expected here
-    
+
     // Then a call to refresh the visualization (if any) would be done here
     this->canvas.queue_draw();
 
-    return true; 
+    return true;
 }
